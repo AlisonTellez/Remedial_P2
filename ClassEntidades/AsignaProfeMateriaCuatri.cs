ec971fc8859eaa7721aecd9d059c4a918e008aa2
Alison Aranza Tellez <aranzateca@gmail.com>
2021-08-13 20:19:23 -0500

    05. En la capa ClassEntidades se creo la clase AsignaProfeMateriaCuatri.

---
+++ ClassEntidades/AsignaProfeMateriaCuatri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntidades
+{
+    public class AsignaProfeMateriaCuatri
+    {
+        public int id_asignapro { get; set; }
+        public int f_profe { get; set; }
+        public int f_materia { get; set; }
+        public int f_grupocuatri { get; set; }
+        public string extra { get; set; }
+    }
+}
