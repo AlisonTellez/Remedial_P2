c4c2acb8cc9fa47b6eff2276bea4de2f29613dc9
Alison Aranza Tellez <aranzateca@gmail.com>
2021-08-15 12:57:09 -0500

    12. En la clase LogicaNegocios, se crearon los métodos actualizar y eliminar del formulario PerfilProfe.

--- ClassLogicaNegocios/LogicaNegocios.cs
+++ ClassLogicaNegocios/LogicaNegocios.cs
@@ -144,6 +144,56 @@ namespace ClassLogicaNegocios
             return salida;
         }//Fin Insertar_GradoEspecialidad.
 
+        //Insertar_PerfilProfe.
+        public Boolean Insertar_PerfilProfe(PerfilProfe nuevo_perfilprofe, ref string msjSalida)
+        {
+            SqlParameter[] param1 = new SqlParameter[5];
+            param1[0] = new SqlParameter
+            {
+                ParameterName = "f_profe",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Input,
+                Value = nuevo_perfilprofe.f_profe
+            };
+            param1[1] = new SqlParameter
+            {
+                ParameterName = "f_grado",
+                SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Input,
+                Value = nuevo_perfilprofe.f_grado
+            };
+            param1[2] = new SqlParameter
+            {
+                ParameterName = "estado",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 150,
+                Direction = ParameterDirection.Input,
+                Value = nuevo_perfilprofe.estado
+            };
+            param1[3] = new SqlParameter
+            {
+                ParameterName = "fecha_obtencion",
+                SqlDbType = SqlDbType.Date,
+                Direction = ParameterDirection.Input,
+                Value = nuevo_perfilprofe.fecha_obtencion
+            };
+            param1[4] = new SqlParameter
+            {
+                ParameterName = "evidencia",
+                SqlDbType = SqlDbType.VarChar,
+                Size = 50,
+                Direction = ParameterDirection.Input,
+                Value = nuevo_perfilprofe.evidencia
+            };
+            string sentenciaSql = "insert into PerfilProfe values(@f_profe,@f_grado,@estado,@fecha_obtencion,@evidencia);";
+
+            Boolean salida = false;
+            salida = cadconex.ModificaBDMasSegura(sentenciaSql, cadconex.AbrirConexion(ref msjSalida), ref msjSalida, param1);
+
+            return salida;
+        }//Fin Insertar_PerfilProfe.
+
+
         //Eliminar_Profesor.
         public string Eliminar_Profesor(string nombre, ref string msjSalida)
         {
@@ -165,6 +215,17 @@ namespace ClassLogicaNegocios
 
             return salida.ToString();
         }//Fin Eliminar_GradoEspecialidad.
+
+        //Eliminar_PerfilProfe.
+        public string Eliminar_PerfilProfe(string evidenciaN, ref string msjSalida)
+        {
+            string sentenciaSql = "delete from  PerfilProfe where Evidencia='" + evidenciaN + "'";
+
+            SqlDataReader salida = null;
+            salida = cadconex.ConsultaReader(sentenciaSql, cadconex.AbrirConexion(ref msjSalida), ref msjSalida);
+
+            return salida.ToString();
+        }//Fin Eliminar_PerfilProfe.
 
         //Actualizar_Profesor.
         public string Actualizar_Profesor(int registroN, string nombreN, string apN, string apmN, string generoN, string categoriaN,
@@ -191,6 +252,18 @@ namespace ClassLogicaNegocios
 
             return salida.ToString();
         }//Fin Actualizar_GradoEspecialidad.
+
+        //Actualizar_PerfilProfe.
+        public string Actualizar_PerfilProfe(int F_profe, int F_grado, string estado, DateTime fecha_obtencion, string evidencia, string evidenciaN, ref string msjSalida)
+        {
+            string sentenciaSql = "update PerfilProfe set F_Profe='" + F_profe + "' ,  F_Grado='" + F_grado + "', Estado='" + estado + "'," +
+             " FechaObtencion='" + fecha_obtencion + "', Evidencia='" + evidencia + "'where evidencia='" + evidenciaN + "'";
+
+            SqlDataReader salida = null;
+            salida = cadconex.ConsultaReader(sentenciaSql, cadconex.AbrirConexion(ref msjSalida), ref msjSalida);
+
+            return salida.ToString();
+        }//Fin Actualizar_PerfilProfe.
 
         //Mostrar datos de la tabla EstadoCivil.
         public List<EstadoCivil> MostrarDatos_EstadoCivil(ref string msjSalida)
@@ -226,5 +299,73 @@ namespace ClassLogicaNegocios
 
             return listaEstadoC;
         }//Mostrar datos de la tabla EstadoCivil.
+
+        //Mostrar datos de la tabla Profesor.
+        public List<Profesor> MostrarDatos_Profesor(ref string msjSalida)
+        {
+            SqlConnection conex = null;
+            string query = "select * from Profesor";
+
+            conex = cadconex.AbrirConexion(ref msjSalida);
+
+            SqlDataReader datos = null;
+            datos = cadconex.ConsultaReader(query, conex, ref msjSalida);
+
+            List<Profesor> listaProfe = new List<Profesor>();
+            if (datos != null)
+            {
+                while (datos.Read())
+                {
+                    listaProfe.Add(new Profesor
+                    {
+                        id_profe = (int)datos[0],
+                        nombre = datos[1].ToString(),
+                    }
+                     );
+                }
+            }
+            else
+            {
+                listaProfe = null;
+            }
+            conex.Close();
+            conex.Dispose();
+
+            return listaProfe;
+        }//Mostrar datos de la tabla Profesor.
+
+        //Mostrar datos de la tabla GradoEspecialidad.
+        public List<GradoEspecialidad> MostrarDatos_GradoEspecialidad(ref string msjSalida)
+        {
+            SqlConnection conex = null;
+            string query = "select * from GradoEspecialidad";
+
+            conex = cadconex.AbrirConexion(ref msjSalida);
+
+            SqlDataReader datos = null;
+            datos = cadconex.ConsultaReader(query, conex, ref msjSalida);
+
+            List<GradoEspecialidad> listaGradoE = new List<GradoEspecialidad>();
+            if (datos != null)
+            {
+                while (datos.Read())
+                {
+                    listaGradoE.Add(new GradoEspecialidad
+                    {
+                        id_grado = (int)datos[0],
+                        institucion = datos[1].ToString(),
+                    }
+                     );
+                }
+            }
+            else
+            {
+                listaGradoE = null;
+            }
+            conex.Close();
+            conex.Dispose();
+
+            return listaGradoE;
+        }//Mostrar datos de la tabla GradoEspecialidad.
     }
 }
